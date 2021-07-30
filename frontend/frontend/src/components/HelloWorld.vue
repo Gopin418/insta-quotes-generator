b4d54f8e2a7484e1c9a4bdfdbea71f9c08cfb2a1
Alvin Ardiansyah <alvin@ehealth.co.id>
2021-07-31 04:06:35 +0700

    first commit

---
+++ frontend/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="test_connection" class="btn btn-primary">Test Koneksi</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  mounted() {
+    this.test_connection();
+  }
+
+  test_connection(): void {
+    const base_url = 'localhost:80';
+    const endpoint = '/api/hello';
+
+    axios.get(`http://${base_url}${endpoint}`)
+    .then((res) => {
+      alert(res);
+    }).catch((error) => {
+      alert(error);
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ quotes_grabber/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/api/hello', methods=['GET'])
+def hello_world():
+
+    response = jsonify(message='Hello, World')
+    response.headers.add("Access-Control-Allow-Origin", "*")
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
